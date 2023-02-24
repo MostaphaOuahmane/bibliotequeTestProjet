d67d6499887bdc0ea8fe841475bc7d596b4c102d
Abdelmounim-bend <104059075+Abdelmounim-bend@users.noreply.github.com>
2023-02-24 16:30:32 +0100

    changement Requette

--- Controller/OuvrageController.php
+++ Controller/OuvrageController.php
@@ -1,11 +1,16 @@
 <?php
 include_once("Model/OuvrageDAo.php");
+
 class OuvrageController {
     /** url: action=ouvragesParMotsCles?motscles=... */
     public static function getByMotsCles() {
         $expressions = filter_input(INPUT_GET, "expressions") or "" ;
-        $motscles = explode(' ', preg_replace('/ +/', ' ', trim($expressions)));
-        $ouvrages= OuvrageDAO::findByMotsCles($motscles);
+        $phrases = explode(',', preg_replace('/,+/', ',', trim($expressions)));
+        $mots_cles = array();
+        foreach ($phrases as $phrase) {
+            $mots_cles[] = preg_replace('/\s+/', '%', trim($phrase));
+        }
+        $ouvrages = OuvrageDAO::findByMotsCles($mots_cles);
         include_once("View/ouvrageV.php");
-    } 
+    }
 }
--- Model/OuvrageDAo.php
+++ Model/OuvrageDAo.php
@@ -1,13 +1,15 @@
-<?php 
-class OuvrageDAO{
-    public static function findByMotsCles($keywords) {      
-        include_once("Model/Pdo_Biblio.php") ;
+<?php
+
+
+class OuvrageDAO {
+    public static function findByMotsCles($mots_cles) {
+        include_once("Model/Pdo_Biblio.php");
         // Colonnes à comparer
         $places = ['o.titre', 'a.prenom', 'a.nom'];
         // tous les something LIKE some keyword 
         $likes = array();
         $keywordIndex = 0;
-        foreach ($keywords as $keyword) {
+        foreach ($mots_cles as $mot_cle) {
             $placeIndex = 0;
             foreach ($places as $place) {
                 array_push($likes,"($place LIKE :keyword$keywordIndex" . "_$placeIndex)");
@@ -24,14 +26,14 @@ class OuvrageDAO{
             auteur a ON o.id_auteur = a.id_auteur
         HAVING nb_occurrences > 0
         ORDER BY nb_occurrences DESC";
-        
+
         // se connecter à bd
         $db = Pdo_Biblio::database();
         $stmt= $db->prepare($sql);
         // Lier les paramètres sur le PDOStatement $stmt
-        for ($kw = 0 ; $kw < count($keywords) ; $kw++) {
+        for ($kw = 0 ; $kw < count($mots_cles) ; $kw++) {
             for ($pl = 0 ; $pl < count($places) ; $pl++) {
-                $stmt->bindValue(":keyword$kw"."_$pl", "%$keywords[$kw]%");
+                $stmt->bindValue(":keyword$kw"."_$pl", "%$mots_cles[$kw]%");
             }
         }  
         $stmt->execute();
